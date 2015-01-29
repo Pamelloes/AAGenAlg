d5bdbf5b92c0a93efc8cf97164f920031a6fbcac
Pamelloes <pamelloes@gmail.com>
2015-01-28 23:42:53 -0600

    Made some improvements to the algorithm. Still highly ineffective.

--- src/main/AAChromosome.hs
+++ src/main/AAChromosome.hs
@@ -9,12 +9,17 @@ import Data.Tuple
 import Language.AA.AdvancedAssembly
 import System.Random
 
-newtype AAProg = AAProg { getAA :: (BitSeries,BitSeries->Double) }
+newtype AAProg = AAProg { getAA :: (DStmt,DStmt->Double) }
+
+bsToDS :: BitSeries -> DStmt
+bsToDS = parseST loadStmt
+
+dsToBS :: DStmt -> BitSeries
+dsToBS = fst . fst
 
 instance NFData RNmspS
 instance NFData Primitive
 instance NFData AAProg
-
 
 rdb :: RandomGen g => g -> (Double,g)
 rdb g = (min/rng,g')
@@ -38,31 +43,47 @@ rbts g i = swap $ runWriter j
         f :: RandomGen g => g -> Writer BitSeries g
         f a = writer $ fmap (:[]) $ swap $ rbt a
 
-genProg :: RandomGen g => Int -> (BitSeries->Double) -> g -> (AAProg,g)
-genProg i f g = (AAProg (b,f),g')
+genProg :: RandomGen g => Int -> (DStmt->Double) -> g -> (AAProg,g)
+genProg i f g = (AAProg (bsToDS b,f),g')
   where (l,g1) = rmx g i
         (b,g') = rbts g1 l
 
-instance Chromosome AAProg where
-  crossover g c d = ([AAProg (b1',f1),AAProg (b2',f2)],g')
-    where (b1,f1) = getAA c
-          (b2,f2)  = getAA d
-          (g1,b1') = combine g b1 b2
-          (g',b2') = combine g1 b1 b2
-          combine :: RandomGen g => g -> [a] -> [a] -> (g,[a])
-          combine g []     _      = (g,[])
-          combine g _      []     = (g,[])
-          combine g (a:as) (b:bs) = fmap (t:) (combine g' as bs)
-            where (c,g') = rbt g
-                  t = if c then a else b
+mutate :: RandomGen g => g -> BitSeries -> (BitSeries,g)
+mutate g b = (b',g')
+  where (s,g2) = rmx g (length b)
+        (a,z) = splitAt s b
+        (r,g3) = (rmx g2 26)
+        (n,g4) = rbts g3 (r-13)
+        (b',g') = if r < 13 then (a++drop r z,g3) else (a++n++z,g4)
 
-  mutation g c = (AAProg (b',f),g')
-    where (b,f) = getAA c
-          (s,g2) = rmx g (length b)
-          (a,z) = splitAt s b
-          (r,g3) = (rmx g2 26)
-          (n,g4) = rbts g3 (r-13)
-          (b',g') = if r < 13 then (a++drop r z,g3) else (a++n++z,g4)
+{-
+combine :: RandomGen g => g -> BitSeries -> BitSeries -> (g,BitSeries)
+combine g []     _      = (g,[])
+combine g _      []     = (g,[])
+combine g a b = fmap ((if c then a' else b')++) (combine g' as bs)
+  where (c,g1) = rbt g
+        (l,g') = rmx g1 25
+        a' = take l a
+        b' = take l b
+        as = drop l a
+        bs = drop l b
+-}
+combine :: RandomGen g => g -> BitSeries -> BitSeries -> (g,BitSeries)
+combine g a b = (g,(take s a)++(drop s b))
+  where l = min (length a) (length b)
+        (s,g') = rmx g l
+
+instance Chromosome AAProg where
+  crossover g (AAProg (d,f)) (AAProg (e,h)) = ([d'],g')
+    where a = dsToBS d
+          b = dsToBS e
+          (g1,a') = combine g a b
+          (g',b') = combine g1 a b
+          d' = AAProg (bsToDS a',f)
+          e' = AAProg (bsToDS b',h)
+  
+  mutation g (AAProg (d,f)) = (AAProg (bsToDS b,f),g')
+    where (b,g') = mutate g $ dsToBS d
 
   fitness a = f b
     where (b,f) = getAA a
--- src/main/Main.hs
+++ src/main/Main.hs
@@ -33,9 +33,11 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 -- This is the main file for AAGenAlg
 module Main where
 
+import qualified GenAlg as Z
+
 import AAChromosome
 import AI.GeneticAlgorithm.Simple
-import Control.Exception
+import Control.Exception (ErrorCall (ErrorCall))
 import Control.Monad.Catch.Pure
 import Control.Monad.Writer
 import Data.Bits
@@ -57,6 +59,12 @@ handleIO a@(_,BString s) = lg a s
           lg d b
 handleIO d = handleIO (cstring d)      
 
+runCond :: Int -> State (WriterT String Catch) 
+               -> WriterT String Catch (State (WriterT String Catch))
+runCond i (S (a,(b,(c,_))))
+  | i > 0 = return $ S (a,(b,(c,runCond (i-1))))
+  | otherwise = lift $ throwM (ErrorCall "Too long!")
+
 up :: Word8 -> [Bit]                                                             
 up w = [h,g,f,e,d,c,b,a]                                                         
   where z a = a /= 0                                                             
@@ -69,23 +77,45 @@ up w = [h,g,f,e,d,c,b,a]
         g=z$w.&.0x40                                                             
         h=z$w.&.0x80 
 
+dist :: Eq a => [a] -> [a] -> Int
+dist a b = last (if lab == 0 then mainDiag
+  else if lab > 0 then lowers !! (lab - 1)
+       else{- < 0 -}   uppers !! (-1 - lab))
+  where mainDiag = oneDiag a b (head uppers) (-1 : head lowers)
+        uppers = eachDiag a b (mainDiag : uppers) -- upper diagonals
+        lowers = eachDiag b a (mainDiag : lowers) -- lower diagonals
+        eachDiag a [] diags = []
+        eachDiag a (bch:bs) (lastDiag:diags) = oneDiag a bs nextDiag lastDiag 
+                                             : eachDiag a bs diags
+          where nextDiag = head (tail diags)
+        oneDiag a b diagAbove diagBelow = thisdiag
+          where doDiag [] b nw n w = []
+                doDiag a [] nw n w = []
+                doDiag (ach:as) (bch:bs) nw n w = me : (doDiag as bs me (tail n)
+                                                        (tail w))
+                  where me = if ach == bch then nw 
+                             else 1 + min3 (head w) nw (head n)
+                firstelt = 1 + head diagBelow
+                thisdiag = firstelt : doDiag a b firstelt diagAbove 
+                                             (tail diagBelow)
+        lab = length a - length b
+        min3 x y z = if x < y then x else min y z
+
 diff :: [Word8] -> [Word8] -> Int
-diff [] b = (-1) * (length b)
-diff a [] = (-1) * (length a)
-diff (a:as) (b:bs) = d + (diff as bs)
-  where d = foldr (+) 0 $ zipWith (\a b->if a==b then 1 else 0) (up a) (up b)
+diff a b = dist (ur a) (ur b)
+  where ur s = mconcat $ fmap up s
 
-runCond :: Int -> State (WriterT String Catch) 
-               -> WriterT String Catch (State (WriterT String Catch))
-runCond i (S (a,(b,(c,_))))
-  | i > 0 = return $ S (a,(b,(c,runCond (i-1))))
-  | otherwise = lift $ throwM (ErrorCall "Too long!")
+runProg :: (Monad m) => (State m -> m (State m))                             
+             -> (DataType -> m DataType) -> DStmt -> m (State m, DataType)   
+runProg j i d = evaluate f s                                                 
+  where p = fst$fst d
+        f = snd d
+        s = S ([],(defaultNamespace p,(i,j)))  
 
-fit :: BitSeries -> Double
+fit :: DStmt -> Double
 fit b = if fail then -1000
-  else if succ then 10001
-    else (fromIntegral df)
-  where c = runCatch $ runWriterT $ runProgram' (runCond 20) handleIO b
+  else if succ then 10001 else 100/(fromIntegral df)
+  where c = runCatch $ runWriterT $ runProg (runCond 50) handleIO b
         fail = case (c) of { Left _ -> True; Right _ -> False }
         Right ((s,dt),st) = c
         desired= "Hello World!"
@@ -97,10 +127,10 @@ finish :: AAProg -> Int -> IO Bool
 finish c i = do
   let score = fitness c
   putStrLn $ "Generation: " ++ show i
-  putStrLn $ "Score: " ++ show score
+  putStrLn $ "Score: " ++ show score ++ "( "++ show (100/score) ++ " )"
   return $ score > 10000
 
 main :: IO ()
 main = do
-  AAProg (p,_) <- runGAIO 50 0.7 (genProg 50 fit) finish
+  AAProg (p,_) <- runGAIO 5 1.0 (genProg 200 fit) finish
   print $ "Program: " ++ show p
